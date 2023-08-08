6a544220a7c3cb12184166833abc9f7148941a9d
Giovani <giakomogcs@gmail.com>
2023-08-07 21:38:39 -0300

    multiplos Layouts

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -7,7 +7,6 @@ public class Index : PageModel{
     public List<Category> Categories { get; set; } = new();
     public async Task OnGet()
     {
-        await Task.Delay(5000);
         for (var i = 0; i <= 100; i++)
         {
             Categories.Add(
