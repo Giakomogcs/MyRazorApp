b51cd9f7000ae7381093833fd1f3d4db2683489c
Giovani <giakomogcs@gmail.com>
2023-08-08 22:14:57 -0300

    finishing aplication

---
+++ Pages/Categories.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyRazorApp.Pages;
+
+public class Categories : PageModel
+{
+    public List<Category> CategoryList { get; set; } = new();
+    public async Task OnGet(int skip = 0, int take = 25)
+    {
+        var temp = new List<Category>();
+        for (var i = 0; i <= 1787; i++)
+        {
+            temp.Add(
+                item:new Category(i, 
+                    Title:$"Categoria {i}", 
+                    Price:i * 18.95M));
+        }
+
+        CategoryList = temp
+            .Skip(skip)
+            .Take(take)
+            .ToList();
+    }
+    
+}
+
+public record Category(int Id, string Title, decimal Price);
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -4,18 +4,9 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace MyRazorApp.Pages;
 
 public class Index : PageModel{
-    public List<Category> Categories { get; set; } = new();
-    public async Task OnGet()
+    public void OnGet()
     {
-        for (var i = 0; i <= 100; i++)
-        {
-            Categories.Add(
-                item:new Category(i, 
-                    Title:$"Categoria {i}", 
-                    Price:i * 18.95M));
-        }
         
     }
 }
 
-public record Category(int Id, string Title, decimal Price);
